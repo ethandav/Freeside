cf126095ea37620fbae3410579303192364adb9c
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2024-06-29 19:50:11 -0700

    Remove D3D12 Sample files

--- efg/Main.cpp
+++ efg/Main.cpp
@@ -1,15 +1,4 @@
-#include "stdafx.h"
-#include "D3D12HelloWindow.h"
 #include "efg.h"
-
-/*
-_Use_decl_annotations_
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
-{
-    D3D12HelloWindow sample(1280, 720, L"D3D12 Hello Window");
-    return Win32Application::Run(&sample, hInstance, nCmdShow);
-}
-*/
 
 int main()
 {
